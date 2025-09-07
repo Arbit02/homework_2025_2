954c6feab1969f4c5e8cdff167f1133ced9aaa64
Ровда Владимир <167581541+Arbit02@users.noreply.github.com>
2025-09-07 09:28:23 +0300

    Update source/transform.js

--- source/transform.js
+++ source/transform.js
@@ -8,7 +8,7 @@
  * // returns { a: 2, b: { c: 3, d: 4 }, e: 5 }
  * transform({ a: 1, b: { c: 2, d: 3 }, e: 4 }, (value) => value + 1);
  *
- * @returns {Object.<string, number>} - преобразованный объект
+ * @returns {Object} - преобразованный объект
  */
 let transform = function(obj, transformFn) {
     for (let key in obj) {
