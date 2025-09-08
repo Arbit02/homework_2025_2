d8a71ab246717c304d335ad7aa47ea9ecab8ef6a
Rovda_V <v_rovda@mail.ru>
2025-09-08 12:53:34 +0300

    Updated tests and solution

--- source/transform.js
+++ source/transform.js
@@ -8,20 +8,19 @@
  * // returns { a: 2, b: { c: 3, d: 4 }, e: 5 }
  * transform({ a: 1, b: { c: 2, d: 3 }, e: 4 }, (value) => value + 1);
  *
- * @returns {Object.<string, number>} - преобразованный объект
+ * @returns {Object} - преобразованный объект
  */
-let transform = function(obj, transformFn) {
-    for (let key in obj) {
-        if(typeof obj[key] === 'object') {
-            transform(obj[key], transformFn);
+const transform = function(obj, transformFn) {
+    if (typeof obj !== 'object') {
+        throw new TypeError('obj must be an object');
+    }
+        const result = Array.isArray(obj) ? [] : {};
+    for (const key in obj) {
+        if (typeof obj[key] === 'object' && obj[key]) {
+            result[key] = transform(obj[key], transformFn);
             continue;
         }
-        obj[key] = transformFn(obj[key]);
+        result[key] = obj[key] ? transformFn(obj[key]) : obj[key] ;
     }
-    return obj;
+    return result;
 }
-
-
-
-
-
