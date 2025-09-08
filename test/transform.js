051c50abd1fa318ec8df0a1692e4fc69da8dde13
Ровда Владимир <167581541+Arbit02@users.noreply.github.com>
2025-09-08 12:56:16 +0300

    Update transform.js
    Добавил тесты с Null и undefined в значениях объекта

--- test/transform.js
+++ test/transform.js
@@ -34,25 +34,21 @@ QUnit.module('Тестируем функцию transform', () => {
         assert.deepEqual(result, { a: 6, b : {c : 17, d : { e: 8, f: { g: 9 } } } }, 'Значения должны быть увеличены на 5');
 
     })
-
     QUnit.test('Работает с пустым объектом', (assert) => {
         const originalObject = {};
         const transformFunction = (value) => value * 4;
         const result = transform(originalObject, transformFunction);
         assert.deepEqual(result, {}, 'Пустой объект должен остаться пустым');
-
     })
     QUnit.test('Обрабатывает null/undefind', (assert) => {
-        const originalObject = null;
+        const originalObject = undefined;
         const transformFunction = (value) => value * 4;
-        const result = transform(originalObject, transformFunction);
-        assert.deepEqual(originalObject, result, 'null должен оставаться null');
+        assert.throws(() => transform(originalObject, transformFunction), TypeError, 'Пустой объект должен остаться пустым');
     })
     QUnit.test('Обрабатывает примитивы', (assert) => {
         const originalObject = 125;
         const transformFunction = (value) => value * 4;
-        const result = transform(originalObject, transformFunction);
-        assert.deepEqual(originalObject, result, 'Пустой объект должен остаться пустым');
+        assert.throws(() => transform(originalObject, transformFunction), TypeError, 'В качестве параметров могут выступать только объекты');
     })
     QUnit.test('Сохраняет иммутабельность объектов', (assert) => {
         const originalObject = { a: [1, 2, 3], b: 4 };
@@ -60,6 +56,18 @@ QUnit.module('Тестируем функцию transform', () => {
         const result = transform(originalObject, transformFunction);
         assert.deepEqual(originalObject, { a: [1, 2, 3], b: 4 }, 'Оригинал не должен измениться');
         assert.deepEqual(result, { a: [3, 6, 9], b: 12 }, 'Результат должен быть правильно преобразован');
+    })
+    QUnit.test('Работает с null в качестве значения', (assert) => {
+        const originalObject = {a: [null, 2, 3], b: 4};
+        const transformFunction = (value) => value * 4;
+        const result = transform(originalObject, transformFunction);
+        assert.deepEqual(result, {a: [null, 8, 12], b: 16}, 'Значения должны быть увеличены в 4 раза, null пропускается');
+    })
+    QUnit.test('Работает с undefined в качестве значения', (assert) => {
+        const originalObject = {a: [undefined, 2, 3], b: 4};
+        const transformFunction = (value) => value * 4;
+        const result = transform(originalObject, transformFunction);
+        assert.deepEqual(result, {a: [undefined, 8, 12], b: 16}, 'Значения должны быть увеличены в 4 раза, undefined пропускается');
     })
 });
 
